acd1b7afa4f7ecdaa53ae0e25373bf1a332b07a0
Dany Entezari <dany.entezari@gmail.com>
2018-08-06 17:48:24 +0400

    Updated code.

--- new-calculator.py
+++ new-calculator.py
@@ -1,19 +1,22 @@
 from tkinter import Tk, Label, Button, Entry
 from pprint import pprint
+import re
 
 class Calculator:
     def __init__(self, window):
         self.window = window
         self.window.title("Calculator")
+        self.total = 0
+        self.arithmeticMode = False
         
-        self.btnLabels = ('AC', '+/-','%','C','7','8','9','x','4','5','6','-','1','2','3','+','0','0','.','=')
+        self.btnLabels = ('AC', '+/-','%','C','7','8','9','x','4','5','6','-','1','2','3','+','0','.','=')
 
         self.window.columnconfigure((0,1,2,3), weight=1)
-        self.window.rowconfigure((0,1), weight=1)
+        self.window.rowconfigure((0,1,2,3,4,5), weight=1)
 
 
         self.label = Label(window,
-            text="The label",
+            text="0",
             highlightthickness=1,
             highlightcolor="black",
             highlightbackground="black",
@@ -27,9 +30,16 @@ class Calculator:
         newCol = 0
         index = 0
 
+        self.window.bind('<Key>', self.keyInput)
+
         for lbl in self.btnLabels:
-            self.button = Button(window, text=lbl, command=self.btnInput)
-            self.button.grid(row=newRow,column=newCol,sticky="nsew")
+            self.button = Button(window, text=lbl)
+            self.button.bind('<Button-1>', self.btnInput)
+            if(lbl == "0"):
+                self.button.grid(row=newRow,column=newCol, columnspan=2, sticky="nsew")
+                newCol+=1
+            else:
+                self.button.grid(row=newRow,column=newCol,sticky="nsew")
 
             index += 1
             newCol += 1
@@ -37,9 +47,33 @@ class Calculator:
                 newRow += 1
                 newCol = 0
 
-    def btnInput(self):
-        self.label.configure(text="A")
-        pprint(self.label)
+    def keyInput(self, event=None):
+        if(re.match('[0-9]',event.keysym)):
+            self.label.configure(text=event.keysym)
+        
+    def btnInput(self, event):
+        lcd = self.label
+        
+        # Clear the screen when the calculator first starts
+        if(lcd["text"] == "0"):
+            lcd["text"] = ""
+
+        # If the plus button is pressed
+        if(event.widget["text"] == "+"):
+            self.arithmeticMode = True
+            self.total = self.total + int(lcd["text"])
+            lcd.configure(text= str(self.total))
+
+        # If any other key is pressed    
+        else:
+            if(self.arithmeticMode == False):
+                lcd.configure(text= lcd["text"] + event.widget["text"])
+            else:
+                lcd.configure(text= event.widget["text"])
+                self.arithmeticMode = False
+
+        pprint(self.total)
+
 
 
 # Create a new TK instance (object) called appWindow
